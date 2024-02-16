f03fe2af8331dfba7fa8af4a6a7e777e27c3ade9
hercjay <iamhercjay@gmail.com>
2024-02-16 12:31:49 +0100

    create basic pages files

---
+++ src/pages/FindShifts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FindShifts = () => {
+  return (
+    <div>FindShifts</div>
+  )
+}
+
+export default FindShifts
---
+++ src/pages/PostShifts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostShifts = () => {
+  return (
+    <div>PostShifts</div>
+  )
+}
+
+export default PostShifts
