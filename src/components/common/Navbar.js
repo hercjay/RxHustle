6fa0270c9e0975de324e842499a64be9a1ec8d6f
hercjay <iamhercjay@gmail.com>
2024-02-17 13:05:14 +0100

    adjust bg colour of navbar to add more contrast

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -21,25 +21,25 @@ const Navbar = () => {
     let [open,setOpen]=useState(false);
 
   return (
-    <nav className="shadow-md w-full sticky top-0 left-0">
+    <nav className="shadow-md w-full sticky top-0 left-0 z-30">
     
-      <div className='md:flex items-center justify-between bg-sky-50 py-4 md:px-10 px-7'>
-        <div className='font-bold text-2xl cursor-pointer flex items-center font-[Titillium Web] 
-        text-gray-800'>
+      <div className='md:flex items-center justify-between bg-sky-100 py-4 md:px-10 px-7'>
+        <Link to='/' className='font-bold text-3xl cursor-pointer flex items-center font-[Titillium Web] 
+        text-sky-700'>
             <span className='text-3xl mr-1 pt-2'>
             {/* <ion-icon name="logo-ionic"></ion-icon> */}
             </span>
             RxHustle
-        </div>
+        </Link>
         
-        <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+        <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-5 cursor-pointer md:hidden'>
         {open ? <Icons.X /> : <Icons.List />}
         </div>
 
-        <ul className={`md:flex md:items-center md:pb-0 pb-2 absolute md:static bg-sky-50  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-14 ':'top-[-490px]'}`}>
+        <ul className={`md:flex md:items-center md:pb-0 pb-2 absolute md:static bg-sky-100  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-14 ':'top-[-490px]'}`}>
             {
             links.map((link, index)=>(
-                <li key={index} className='md:ml-8 text-xl md:my-0 my-7 font-[Titillium Web]' onClick={()=>setOpen(false)}>
+                <li key={index} className='md:ml-8 text-xl md:my-0 my-4 font-[Titillium Web]' onClick={()=>setOpen(false)}>
                     <Link to={link.url} className='text-gray-800 hover:text-sky-500  transition all duration-500 font-[Titillium Web]'>{link.name}</Link>
                 </li>
             ))
