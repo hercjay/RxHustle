5527604b766cdf33250616fa9c92e4290180e277
hercjay <iamhercjay@gmail.com>
2024-02-17 13:09:11 +0100

    add shifts list and SEE ALL SHIFTS button on the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,148 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { places } from '../constants/places.js';
+import {shifts} from '../constants/shifts.js';
+import ShiftCard from '../components/ShiftCard/ShiftCard.js';
+import MyDatePicker from '../components/common/MyDatePicker.js';
+import MyButton from '../components/common/MyButton.js';
+import { Link } from 'react-router-dom';
+
+
+
 
 const Home = () => {
 
-    let places = [
-        'Abule Egba',
-        'Abule-Egba',
-        'Abule-Oja',
-        'Agege',
-        'Agidingbi',
-        'Aguda',
-        'Agungi',
-        'Ajah',
-        'Ajao Estate',
-        'Ajegunle',
-        'Akerele',
-        'Akoka',
-        'Akowonjo',
-        'Alagbado',
-        'Alakuko',
-        'Alapere',
-        'Alausa',
-        'Alimosho',
-        'Amuwo-Odofin',
-        'Anthony Village',
-        'Apapa',
-        'Awolowo Road',
-        'Badagry',
-        'Bariga',
-        'Bourdillon',
-        'Coker',
-        'Dolphin Estate',
-        'Dopemu',
-        'Dopemu-Agege',
-        'Ebutte Metta',
-        'Egbeda',
-        'Egbeda-Idimu',
-        'Egba',
-        'Ejigbo',
-        'Ekoro',
-        'Epe',
-        'Fadeyi',
-        'Festac Town',
-        'Gbagada',
-        'Gboko',
-        'Iba',
-        'Ibeju-Lekki',
-        'Idimu',
-        'Igando',
-        'Igbo Efon',
-        'Igbobi College',
-        'Igbogbo',
-        'Ikeja',
-        'Ikeja Gra',
-        'Ikorodu',
-        'Ikosi Ketu',
-        'Ikota',
-        'Ikotun',
-        'Ikoyi',
-        'Ilasamaja',
-        'Ilashe',
-        'Ilupeju',
-        'Ipaja',
-        'Ipakodo',
-        'Iponri',
-        'Irawo',
-        'Isolo',
-        'Jibowu',
-        'Ketu',
-        'Kosofe',
-        'Kuramo Beach',
-        'Lagos Island',
-        'Lagos Mainland',
-        'Lakowe',
-        'Lekki',
-        'Lekki Phase 1',
-        'Lekki Phase 2',
-        'Lekki Peninsula',
-        'Magodo',
-        'Makoko',
-        'Marina',
-        'Maryland',
-        'Mende',
-        'Mile 12',
-        'Mushin',
-        'Obalende',
-        'Obanikoro',
-        'Ogba',
-        'Ogudu',
-        'Ojodu',
-        'Ojota',
-        'Ojuelegba',
-        'Ojota',
-        'Okokomaiko',
-        'Okota',
-        'Ologolo',
-        'Onike',
-        'Oniru',
-        'Oniru Estate',
-        'Opebi',
-        'Oregun',
-        'Orile',
-        'Oshodi',
-        'Oworonshoki',
-        'Palmgrove',
-        'Palmgrove Estate',
-        'Sabo',
-        'Satellite Town',
-        'Shomolu',
-        'Surulere',
-        'Takwa Bay',
-        'Tejuosho',
-        'Victoria Garden City',
-        'Victoria Island',
-        'Yaba',
-        'Yaba College Of Technology'
-    ];
-    
-    
+    const [selectedDate, setSelectedDate] = useState(new Date());
 
     return (
-        <div className='bg-slate-100'>
+        <div className='bg-slate-100 px-5 md:px-20'>
             
-            <h2 className="font-bold p-2 text-4xl text-teal-700 text-center pt-10">
+            <h2 className="font-bold text-4xl text-sky-700 text-center pt-10">
                 The perfect place for you to find or post shifts!
             </h2>
 
-            <div className='md:px-20 px-5 my-10'>
+            <p className="text-lg text-gray-600 mt-4 leading-relaxed text-start">
+                Welcome to RxHustle, the perfect platform for you to discover and advertise shifts in your area. Whether you're a Pharmacist seeking opportunities or a Pharmacy in need of staffing solutions, RxHustle provides a seamless experience to connect you with the solution you need. <br/>Browse through a wide range of shifts available in your locality or effortlessly post your vacancies to attract qualified professionals. <br/>Join our community today and simplify your shift management process with RxHustle.
+            </p>
+
+
+            <div className='my-10'>
                 <div className='bg-white rounded-lg p-4 grid grid-cols-1 md:grid-cols-3 gap-4 items-center'>
                     
-                        <select className='mb-4 md:mb-0 md:mr-2 p-2 rounded border border-gray-300 w-100'>
-                            {/* Dropdown options for date */}
-                        </select>
-                        <select className='w-full mb-4 md:mb-0 p-2 rounded border border-gray-300'>
+                        <MyDatePicker selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+                        <select className='w-full mb-2 md:mb-0 p-2 rounded border border-gray-300'>
                             {places.map((place, index)=>(
                                 <option key={index} value={place}>{place}</option>
                             ))}
                         </select>
-                        <button className='bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded'>
-                            Find Shift!
-                        </button>
-                    
+                        
+                        <MyButton text='Find Shifts' />
                     
                 </div>
             </div>
+
+            <div className='my-10 grid grid-cols-2 md:grid-cols-4 gap-4'>
+                {/* display only the first eith shifts */}
+                {shifts.slice(0, 8).map((shift, index) => (
+                    <ShiftCard key={index} shift={shift} />
+                ))}
+            </div>
+
+            <Link to='/find-shifts' className='grid grid-cols-1 md:grid-cols-3'>
+                <div></div>
+                <MyButton text='See All Shifts' />
+                <div></div>
+            </Link>
 
         </div>
     )
