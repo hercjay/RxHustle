ff9abb079e7fb348316eeaddac0014d40958bc12
hercjay <iamhercjay@gmail.com>
2024-02-20 18:49:17 +0100

    create basic logout js file

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import PharmacistController from '../features/Pharmacist/PharmacistController';
 
 const Logout = () => {
+  const pharmacistController = new PharmacistController();
+  pharmacistController.logOut();
   return (
     <div>Logout</div>
   )
