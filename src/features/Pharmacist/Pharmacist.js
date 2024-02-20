721ad49d8c3baa2423eae203df89ecb30d55335a
hercjay <iamhercjay@gmail.com>
2024-02-20 18:44:59 +0100

    update pharmacist user model to have the role field

--- src/features/Pharmacist/Pharmacist.js
+++ src/features/Pharmacist/Pharmacist.js
@@ -2,14 +2,14 @@ class Pharmacist {
   constructor({ id, firstname, lastname, email, phoneNumber, address, password, photo, availability, shifts }) {
     this.id = id;
     this.firstname = firstname;
-    this.lastname = lastname;
+    this.lastname = lastname ?? '';
     this.email = email;
-    this.phoneNumber = phoneNumber;
+    this.phoneNumber = phoneNumber ?? '';
     this.address = address;
-    this.password = password;
-    this.photo = photo;
-    this.availability = availability;
-    this.shifts = shifts;
+    this.password = password ?? '';
+    this.photo = photo ?? '';
+    this.availability = availability ?? [];
+    this.shifts = shifts ?? [];
     this.role = 'pharmacist';
   }
 }
