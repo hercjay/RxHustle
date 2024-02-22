6660b529251a6a45f6cc4963c1f1bb47e7fbde29
hercjay <iamhercjay@gmail.com>
2024-02-22 14:31:08 +0100

    include user and setuser values on the values exposed by the loading context globally

--- src/context/LoadingContext.js
+++ src/context/LoadingContext.js
@@ -4,9 +4,10 @@ export const LoadingContext = createContext();
 
 export const LoadingProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
 
   return (
-    <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
+    <LoadingContext.Provider value={{ isLoading, setIsLoading, user, setUser }}>
       {children}
     </LoadingContext.Provider>
   );
