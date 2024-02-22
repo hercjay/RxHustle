80706faf577d4863bdd66260e532693c828d7f5c
hercjay <iamhercjay@gmail.com>
2024-02-22 14:32:25 +0100

    update logout code to set user to null and redirect to home

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PharmacistController from '../features/Pharmacist/PharmacistController';
+import { LoadingContext } from '../context/LoadingContext';
+import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
+  const { setUser } = useContext(LoadingContext);
+  const navigate = useNavigate();
   const pharmacistController = new PharmacistController();
-  pharmacistController.logOut();
-  return (
-    <div>Logout</div>
-  )
+  pharmacistController.logOut().then((user) => {
+    setUser(null);
+    navigate('/');
+  });
+  
 }
 
 export default Logout
