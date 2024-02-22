5537f69e525efbba055e08567e7ea5c69f43e485
hercjay <iamhercjay@gmail.com>
2024-02-22 16:39:24 +0100

    Let post shift page redirect to signin when no user is logged in

--- src/pages/PostShifts.js
+++ src/pages/PostShifts.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { LoadingContext } from '../context/LoadingContext';
+import { handleGoogleSignIn } from './Signup';
+import { Google as GoogleIcon } from 'react-bootstrap-icons';
+import { useNavigate } from 'react-router-dom';
 
 const PostShifts = () => {
+
+  const { user } = useContext(LoadingContext);
+  const [ shouldRedirect, setShouldRedirect ] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user == null) {
+      setShouldRedirect(true);
+    } else { 
+      setShouldRedirect(false);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (shouldRedirect) {
+      navigate('/signup');
+    }
+  }, [shouldRedirect]);
+
   return (
-    <div>PostShifts</div>
+    <div>
+        Post Shifts
+    </div>
   )
 }
 
