0816766682090a34a14202354dc10b71f221bc6a
hercjay <iamhercjay@gmail.com>
2024-02-23 20:59:40 +0100

    obtain currently logged in user data from localstorage in the loading context provider and make the user available throughout the app

--- src/context/LoadingContext.js
+++ src/context/LoadingContext.js
@@ -1,10 +1,24 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import PharmacistController from '../features/Pharmacist/PharmacistController';
 
 export const LoadingContext = createContext();
 
 export const LoadingProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
+  const pharmacistController = new PharmacistController();
+
+
+  useEffect(() => {
+    pharmacistController.getLoggedInPharmacist().then((pharmacist) => {
+      setUser(pharmacist);
+      setIsLoading(false);
+      console.log('Logged in pharmacist: ', pharmacist);
+    }).catch((error) => {
+      console.log('Error getting logged in pharmacist: ', error);
+      setIsLoading(false);
+    });
+  }, []);
 
   return (
     <LoadingContext.Provider value={{ isLoading, setIsLoading, user, setUser }}>
