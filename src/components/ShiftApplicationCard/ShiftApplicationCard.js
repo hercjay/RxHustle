92439af2d9467aaa9a89a2b96326ea8c9a77dcef
hercjay <iamhercjay@gmail.com>
2024-02-25 16:18:45 +0100

    add approve shift and reject shift handlers to update shift application lists in realtime

--- src/components/ShiftApplicationCard/ShiftApplicationCard.js
+++ src/components/ShiftApplicationCard/ShiftApplicationCard.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import MyButton from '../common/MyButton'
 import MySecondaryButton from '../common/MySecondaryButton'
 import MyTertiaryButton from '../common/MyTertiaryButton'
+import ShiftController from '../../features/Shift/ShiftController'
+import { LoadingContext } from '../../context/LoadingContext'
 
-const ShiftApplicationCard = ({application, user}) => {
+const ShiftApplicationCard = ({application, user, moveToRejected, moveToApproved, isPending }) => {
+
+    const { 
+        isLoading, setIsLoading, isShowToast, setIsShowToast,
+        toastType, setToastType, toastMessage, setToastMessage } = useContext(LoadingContext);
+
+    const shiftController = new ShiftController();
+
+    const approveShiftApplication = () => {
+        setIsLoading(true);
+        shiftController.approveShiftApplication(application).then((response) => {
+            setIsLoading(false);
+            moveToApproved(application);
+            setToastType('success');
+            setToastMessage('Shift application approved successfully. Do NOT forget to reach out to the user via mail for further communication');
+            setIsShowToast(true);
+        }).catch((error) => {
+            setIsLoading(false);
+            setToastType('error');
+            setToastMessage('Error approving shift application. Try again later');
+            setIsShowToast(true);
+        });
+    };
+
+    const rejectShiftApplication = () => {
+        setIsLoading(true);
+        shiftController.rejectShiftApplication(application).then((response) => {
+            setIsLoading(false);
+            moveToRejected(application);
+            setToastType('success');
+            setToastMessage('Shift application rejected successfully');
+            setIsShowToast(true);
+        }).catch((error) => {
+            setIsLoading(false);
+            setToastType('error');
+            setToastMessage('Error rejecting shift application. Try again later');
+            setIsShowToast(true);
+        });
+    };
+
 
     const subject = `Regarding Your Shift Application for ${application.shiftDate} on RxHustle`;
     const body = `Dear Pharm. ${application.applicantName},
@@ -35,19 +76,25 @@ const ShiftApplicationCard = ({application, user}) => {
             <p className="text-sm font-normal text-slate-500">Shift Start Time: {application.shiftStartTime}</p>
             <p className="text-sm font-normal text-slate-500">Shift End Time: {application.shiftEndTime}</p>
             <p className="text-sm font-normal text-slate-500">Applicant: <strong>{application.applicantName}</strong> ({application.applicantId})</p>
-            <div className="flex justify-end gap-2 pt-4">
-                {/* <a href={`mailto:${application.applicantId}`}>
-                    <MyButton text="Mail Applicant (Default)" />
-                </a> */}
-                <a title='Reachout to user to discuss further details'
-                    href={`https://mail.google.com/mail/u/0/?view=cm&fs=1&to=${application.applicantId}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}&tf=1`} target="_blank" rel="noopener noreferrer">
-                    <MyButton text="Mail User (Gmail)" />
-                </a>
-                <a title='Do not forget to reach out to the user via mail after approving their application for further communication'>
-                    <MyTertiaryButton text="Approve" />
-                </a>
-                <MySecondaryButton text="Reject" />
-            </div>
+            {
+                isPending &&
+                <div className="flex justify-end gap-2 pt-4">
+                    {/* <a href={`mailto:${application.applicantId}`}>
+                        <MyButton text="Mail Applicant (Default)" />
+                    </a> */}
+                    <a title='Reachout to user to discuss further details'
+                        href={`https://mail.google.com/mail/u/0/?view=cm&fs=1&to=${application.applicantId}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}&tf=1`} target="_blank" rel="noopener noreferrer">
+                        <MyButton text="Mail User (Gmail)" />
+                    </a>
+                    <a onClick={approveShiftApplication}
+                        title='Do not forget to reach out to the user via mail after approving their application for further communication'>
+                        <MyTertiaryButton text="Approve" />
+                    </a>
+                    <a onClick={rejectShiftApplication} >
+                        <MySecondaryButton text="Reject" />
+                    </a>
+                </div>
+            }
         </div>
     </div>
   )
